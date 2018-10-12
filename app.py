567609923d85cbff9d656f00c15cf29d14584a51
wulening <wulening@foxmail.com>
2018-10-12 20:31:29 +0800

    第二次课

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 
-from flask import Flask, render_template, request , make_response ,redirect
+from flask import Flask, render_template, request , make_response ,redirect , flash ,get_flashed_messages
 
 app = Flask(__name__)
 app.jinja_env.line_statement_prefix = '#'
-
+app.secret_key='noecoder'
 
 @app.route('/index/')
 @app.route('/')
 def index():
+    res = ''
+    for msg in get_flashed_messages():
+        res = res + msg + '<br>'
+    res += 'hello'
     return 'Hello World!'
 
 
@@ -23,10 +27,10 @@ def request_demo():
     res = request.args.get('key', 'defaultkey') + '<br>'
     res =res + request.url+'++'+request.path + '<br>'
     for property in dir(request):
-        res =res+str(property)+'|==|<br>' + str(eval('request.' + property)) + '<br>'
+        res = res + str(property) +'|==|<br>' + str(eval('request.' + property)) + '<br>'
     response=make_response(res)
-    response.set_cookie('nowcoderid',key)
-    response.status='404'
+    response.set_cookie('nowcoderid' , key)
+    response.status = '404'
     return response
 
 @app.route('/newpath')
@@ -36,6 +40,17 @@ def newpath():
 @app.route('/re/<int:code>')
 def redirect_demo(code):
     return redirect('newpath',code=code)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    print(error)
+    return render_template('not_found.html', url=request.url)
+
+@app.route('/login')
+def login():
+    flash('登录成功 ')
+    return redirect('/')
 
 
 if __name__ == '__main__':
